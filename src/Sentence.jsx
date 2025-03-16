ec7e4bfa572a79d6d474df1ce55f3956c3648b29
Lukas Moro <lukas.moro@me.com>
2025-03-16 21:21:41 +0100

    initial commit

---
+++ src/Sentence.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sentence.css';
+
+function Sentence() {
+  return (
+    <div className="sentence">
+      Tiny Software solves Tiny Problems with Tiny Products.
+    </div>
+  );
+}
+
+export default Sentence;
