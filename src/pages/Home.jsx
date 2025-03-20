a1c3c27841df8bcc73df1347202375013483d028
Lukas Moro <lukas.moro@me.com>
2025-03-20 15:13:13 +0100

    hello world

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Sentence from '../components/Sentence';
+import Socials from '../components/Socials';
+import './Home.css';
 
 function Home() {
   return (
+    <div className="home-container">
       <Sentence />
+      <Socials />
+    </div>
   );
 }
 
