0a946b6a29640adf31b81ab262a1c43c0051be3a
Lukas Moro <lukas.moro@me.com>
2025-04-10 18:29:35 +0200

    add inter font and enhance media component with download button and podcast images

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import './Media.css';
+import tinypodcasts1 from '../assets/tinypodcasts1.jpg';
+import tinypodcasts2 from '../assets/tinypodcasts2.jpg';
+import tinypodcasts3 from '../assets/tinypodcasts3.jpg';
+import tinypodcasts4 from '../assets/tinypodcasts4.jpg';
+import tinypodcasts5 from '../assets/tinypodcasts5.jpg';
+import tinypodcasts6 from '../assets/tinypodcasts6.jpg';
 
 function Media() {
+  const handleDownloadTinyPodcasts = () => {
+    window.open('https://chromewebstore.google.com/detail/tiny-podcasts/mleolcoofjackgkblpolibcdbcdbpohf', '_blank', 'noopener,noreferrer');
+  };
+
   return (
     <div className="media-container">
       <div className="media-content">
-        {/* Media content goes here */}
+        <div className="podcast-column">
+          <img src={tinypodcasts1} alt="Tiny Podcast 1" className="podcast-image" />
+          <img src={tinypodcasts2} alt="Tiny Podcast 2" className="podcast-image" />
+          <img src={tinypodcasts3} alt="Tiny Podcast 3" className="podcast-image" />
+          <img src={tinypodcasts4} alt="Tiny Podcast 4" className="podcast-image" />
+          <img src={tinypodcasts5} alt="Tiny Podcast 5" className="podcast-image" />
+          <img src={tinypodcasts6} alt="Tiny Podcast 6" className="podcast-image" />
+        </div>
+        <button className="download-button" onClick={handleDownloadTinyPodcasts}>
+          Download from Chrome Web Store.
+        </button>
       </div>
     </div>
   );
